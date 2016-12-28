76c100ec9625fff4766754ae742ca5b285f57abf
ifdog <ifdog@live.cn>
2016-12-28 22:04:52 +0800

    Update

---
+++ Queries/Service/Model/BoxDb.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using iBoxDB.LocalServer;
+
+namespace Service.Model
+{
+    public class BoxDb<T> : IDataBase<T> ,IDisposable where T : class, new()
+    {
+        private readonly DB _databBase = new DB();
+        private readonly AutoBox _autoBox;
+        private readonly string _itemName;
+        public BoxDb()
+        {
+            _itemName = typeof(T).Name;
+            DB.Root("/data/");
+            _databBase.GetConfig().EnsureTable<T>(_itemName, "Id");
+            _autoBox = _databBase.Open();
+        }
+
+        public void Insert(T obj)
+        {
+            _autoBox.Insert(_itemName, obj);
+        }
+
+        public IEnumerable<T> Select()
+        {
+            return _autoBox.Select<T>($"from {_itemName}");
+        }
+
+        public void Update(T obj)
+        {
+            _autoBox.Update(_itemName, obj);
+        }
+
+        public void Delete(T obj)
+        {
+            _autoBox.Delete(_itemName, obj);
+        }
+
+        public void Dispose()
+        {
+            _databBase.Close();
+        }
+    }
+}
---
+++ Queries/Service/Model/IDataBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Model
+{
+    public interface IDataBase<T> where T : class
+    {
+        void Insert(T obj);
+        IEnumerable<T> Select();
+        void Update(T obj);
+        void Delete(T obj);
+    }
+}

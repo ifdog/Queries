21d7f55d50892483b3214dd6425cc4bf12334401
ifdog <ifdog@live.cn>
2017-01-02 15:30:43 +0800

    Update

---
+++ Queries/Queries/View/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Queries.View
+{
+	/// <summary>
+	/// MainWindow.xaml 的交互逻辑
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
--- Queries/Queries/View/QueryWindow.xaml.cs
+++ Queries/Queries/View/QueryWindow.xaml.cs
@@ -10,19 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Queries
 {
-	/// <summary>
-	/// MainWindow.xaml 的交互逻辑
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
-	}
+    /// <summary>
+    /// Interaction logic for QueryWindow.xaml
+    /// </summary>
+    public partial class QueryWindow : Window
+    {
+        public QueryWindow()
+        {
+            InitializeComponent();
+        }
+    }
 }

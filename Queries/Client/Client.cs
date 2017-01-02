37ec5fff20a23aae7584def48c5a4085406cbd45
ifdog <ifdog@live.cn>
2017-01-02 15:10:52 +0800

    Update

--- Queries/Client/Client.cs
+++ Queries/Client/Client.cs
@@ -1,22 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
+using System.Resources;
 using System.Text;
-using System.Threading.Tasks;
+using Client.Bll;
 using RestSharp;
 
 namespace Client
 {
     public class Client
     {
-        private RestClient _restClient;
-        private 
+        private readonly RestClient _restClient;
+        private ResourceManager _resourceManager;
+
+        public UserBll User { get; private set; }
+        public ItemBll Item { get; private set; }
 
         public Client(string serviceDomain)
         {
-            _restClient = new RestClient(serviceDomain);
-            _restClient.CookieContainer = new CookieContainer();
+            _resourceManager = new ResourceManager("Client.Resources", typeof(Client).Assembly);
+            _restClient = new RestClient(serviceDomain)
+            {
+                CookieContainer = new CookieContainer(),
+                Encoding = Encoding.UTF8,
+                BaseUrl = new Uri(serviceDomain),
+                FollowRedirects = true,
+                UserAgent = _resourceManager.GetString("UserAgent"),
+                Timeout = 10000
+            };
+            User = new UserBll(_restClient);
+            Item = new ItemBll(_restClient);
         }
+
     }
 }

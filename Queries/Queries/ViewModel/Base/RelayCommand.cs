a7b0023157188d3372a0c34b014f9c7f2186a1bb
ifdog <ifdog@live.cn>
2017-01-02 22:07:53 +0800

    Update

---
+++ Queries/Queries/ViewModel/Base/RelayCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Queries.ViewModel.Base
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        private readonly Action _methodToExecute;
+        private readonly Func<bool> _canExecuteEvaluator;
+
+        public RelayCommand(Action methodToExecute, Func<bool> canExecuteEvaluator)
+        {
+            this._methodToExecute = methodToExecute;
+            this._canExecuteEvaluator = canExecuteEvaluator;
+        }
+
+        public RelayCommand(Action methodToExecute)
+            : this(methodToExecute, null)
+        {
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (this._canExecuteEvaluator == null)
+            {
+                return true;
+            }
+            else
+            {
+                var result = this._canExecuteEvaluator.Invoke();
+                return result;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            this._methodToExecute.Invoke();
+        }
+    }
+}
---
+++ Queries/Queries/ViewModel/Base/ViewModelBase.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace Queries.ViewModel.Base
+{
+    public class ViewModelBase<TViewType> : INotifyPropertyChanged
+    where TViewType : Window
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public TViewType View { get; }
+
+        public ViewModelBase(TViewType view)
+        {
+            this.View = view;
+            this.View.DataContext = this;
+        }
+        public void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

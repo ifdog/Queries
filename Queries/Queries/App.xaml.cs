0909d048b4bd8782731ae9341b06d62823df0a50
ifdog <zouyuanpeng@163.com>
2017-01-03 09:37:15 +0800

    Update

--- Queries/Queries/App.xaml.cs
+++ Queries/Queries/App.xaml.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using Queries.View;
 
 namespace Queries
 {
@@ -13,5 +8,19 @@ namespace Queries
 	/// </summary>
 	public partial class App : Application
 	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			Current.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+			var dialogResult = new LoginWindow().ShowDialog();
+			if (dialogResult.HasValue && dialogResult.Value)
+			{
+				base.OnStartup(e);
+				Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
+			}
+			else
+			{
+				Shutdown();
+			}
+		}
 	}
 }

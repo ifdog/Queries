7b97fcd8a3a9c8782e8f99953db8c7476f75e3b3
ifdog <zouyuanpeng@163.com>
2017-01-09 15:56:53 +0800

    Update

--- Queries/Common/Structure/CsvModel.cs
+++ Queries/Common/Structure/CsvModel.cs
@@ -4,21 +4,21 @@ namespace Common.Structure
 {
 	public class CsvModel
 	{
-		[Column(Index = 1,Title = "名称")]
+		[Column(Index = 0,Title = "名称")]
 		public string Name { get; set; }
-		[Column(Index = 2, Title = "型号")]
+		[Column(Index = 1, Title = "型号")]
 		public string Model { get; set; }
-		[Column(Index = 3, Title = "规格")]
+		[Column(Index = 2, Title = "规格")]
 		public string Spec { get; set; }
-		[Column(Index = 4, Title = "品牌")]
+		[Column(Index = 3, Title = "品牌")]
 		public string Brand { get; set; }
-		[Column(Index = 5, Title = "供应商")]
+		[Column(Index = 4, Title = "供应商")]
 		public string Supplier { get; set; }
-		[Column(Index = 6, Title = "备注")]
+		[Column(Index = 5, Title = "备注")]
 		public string Remark { get; set; }
-		[Column(Index = 7, Title = "折扣")]
+		[Column(Index = 6, Title = "折扣")]
 		public string Discount { get; set; }
-		[Column(Index = 8, Title = "表价")]
+		[Column(Index = 7, Title = "表价")]
 		public string OriginPrice { get; set; }
 	}
 }

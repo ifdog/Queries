cf736ccff27be9fc697a1dfc3f99bd5c10accd32
ifdog <zouyuanpeng@163.com>
2017-01-11 15:59:31 +0800

    Update

---
+++ Queries/Common/Factory/Hotkey.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trustsoft.WideHotkeys;
+using System.Windows.Input;
+
+namespace Common.Factory
+{
+	public class Hotkey
+	{
+		public void Register()
+		{
+		}
+	}
+}

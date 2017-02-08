6b1928afd2159be11e00f13d013a91366ac5c9c5
ifdog <zouyuanpeng@163.com>
2017-02-08 12:04:49 +0800

    Update

--- Queries/Service/Dal/PgDal.cs
+++ Queries/Service/Dal/PgDal.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Npgsql;
 using Service.Dal.Base;
 
 namespace Service.Dal
 {
 	public class PgDal<T>:IDal<T>
 	{
+		internal Npgsql.NpgsqlConnection Connection;
+		public PgDal()
+		{
+			Connection = new NpgsqlConnection("Server=127.0.0.1;Port=5432;User Id=Queries;Password=Queries;Database=Queries;");
+			Connection.Open();
+			Connection.
+		}
 		public void Insert(T obj)
 		{
 			throw new NotImplementedException();

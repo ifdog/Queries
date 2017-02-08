45924b5f16fdaa7b9ab35f6e3fef9ca7fc204869
ifdog <zouyuanpeng@163.com>
2017-02-08 10:56:18 +0800

    Update

---
+++ Queries/Service/Dal/PgDal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Service.Dal.Base;
+
+namespace Service.Dal
+{
+	public class PgDal<T>:IDal<T>
+	{
+		public void Insert(T obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(T obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete(T obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<T> Find(string query, int page, int length)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
